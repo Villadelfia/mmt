0eef517fd08e888c4a85012b3e1504f77e5e056a
Randy Thiemann <uselinuxnow@gmail.com>
2013-10-04 19:36:29 +0200

    testcases

---
+++ Practicum02/src/encoder.cpp
@@ -0,0 +1,15 @@
+/**
+ * Randy Thiemann 2013
+ **/
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        std::cout << "Usage: " << argv[0] << " settings.conf" <<std::endl;
+        return 1;
+    }
+
+    // Kick off process here.
+}
+
